c573541684cbbe24c563750ae61e526fab9530db
Nayzus <Nayzus@list.ru>
2020-08-28 22:06:47 +0700

    New

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+    $(document).ready(function(e){
+        var left = 0,
+            top = 0,
+            element = $('#header'),
+            offset =  { left: element.offset().left, top: element.offset().top };
+        element.bind('mousemove', function(e){
+            left = (e.pageX-offset.left);
+            top = (e.pageY-offset.top);
+            $(this).css({
+                backgroundPosition: '-'+(0.1*left)+'px -'+(0.05*top)+'px'
 
+            });
+        });
+    });
 
 });
